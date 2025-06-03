4d640bbede04f663a142e988c517ff57f2d058c6
جمال عبد الناصر الهويش <gamalalmaqtary6838@gmail.com>
2025-06-03 04:31:00 +0000

    Project Id is learn-5-by-5, project api key is AIzaSyDeN1mxcNwQqOyBtLE2AgZoBzf5exPYBoc

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -10,10 +10,10 @@ import { getFirestore, type Firestore } from 'firebase/firestore'; // Added Fire
 // Firebase Authentication (especially social logins) will NOT work without this.
 const firebaseConfig = {
   apiKey: "AIzaSyDeN1mxcNwQqOyBtLE2AgZoBzf5exPYBoc",
-  authDomain: "helplearn-5-by-5.firebaseapp.com",
-  projectId: "helplearn-5-by-5",
-  storageBucket: "helplearn-5-by-5.appspot.com",
-  messagingSenderId: "434056178407",
+  authDomain: "learn-5-by-5.firebaseapp.com", // Updated based on new projectId
+  projectId: "learn-5-by-5",                // Updated projectId
+  storageBucket: "learn-5-by-5.appspot.com", // Updated based on new projectId
+  messagingSenderId: "434056178407",       // From previous update
   appId: "YOUR_APP_ID_HERE" // Please get this from your Firebase project settings
 };
 
